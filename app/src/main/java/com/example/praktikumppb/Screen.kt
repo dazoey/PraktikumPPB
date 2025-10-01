50cc734e8fbdd7e878584633d1a6ba64fb7c1d60
dazoey <151373427+dazoey@users.noreply.github.com>
2025-10-01 14:02:17 +0700

    apdet

--- app/src/main/java/com/example/praktikumppb/Screen.kt
+++ app/src/main/java/com/example/praktikumppb/Screen.kt
@@ -3,5 +3,8 @@ package com.example.praktikumppb
 sealed class Screen(val route: String, val title: String) {
     object Anime : Screen("anime", "Anime")
     object About : Screen("about", "About")
+    object AnimeDetail : Screen("anime/{id}", "Anime Detail")
+    object Characters : Screen("characters", "Characters")
+    object CharacterDetail : Screen("character/{id}", "Character Detail")
 }
 
--- app/src/main/java/com/example/praktikumppb/network/ApiService.kt
+++ app/src/main/java/com/example/praktikumppb/network/ApiService.kt
@@ -1,10 +1,27 @@
 package com.example.praktikumppb.network
 
 import com.example.praktikumppb.model.AnimeListResponse
+import com.example.praktikumppb.model.AnimeResponse
+import com.example.praktikumppb.model.CharacterListResponse
+import com.example.praktikumppb.model.CharacterResponse
+import com.example.praktikumppb.model.GenreListResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
-    // Anime by id
     @GET("top/anime")
-    suspend fun getTopAnime(): AnimeListResponse
+    suspend fun getTopAnime(@Query("genres") genres: String? = null): AnimeListResponse
+
+    @GET("anime/{id}")
+    suspend fun getAnimeById(@Path("id") id: Int): AnimeResponse
+
+    @GET("top/characters")
+    suspend fun getTopCharacters(): CharacterListResponse
+
+    @GET("characters/{id}")
+    suspend fun getCharacterById(@Path("id") id: Int): CharacterResponse
+
+    @GET("genres/anime")
+    suspend fun getAnimeGenres(): GenreListResponse
 }
